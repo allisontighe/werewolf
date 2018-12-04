230c4432e3303b65a06c78a00a97d8e45093c7fe
ArcTLK <adityanair313@gmail.com>
2018-12-04 17:55:16 +0530

    error handling

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
+require_once 'Database.class.php';
 require_once 'config.php';
 require_once 'WerewolfBot.class.php';
+function errorHandle($no, $e, $file, $line) {
+    (new Database())->log($e.$file.$line);
+}
+error_reporting(E_ERROR & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+ini_set('display_errors', 'On');
+set_error_handler('errorHandle');
+session_start();
+
 $content = file_get_contents('php://input');
 $update = json_decode($content, true);
 if (!$update) {
