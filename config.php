fd1aa89416417fb00aadde380b309afa129cfe0f
ArcTLK <adityanair313@gmail.com>
2018-12-04 19:53:10 +0530

    updated token

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 abstract class Config {
-    public const BOT_TOKEN = '729205038:AAGwSF2HV4sELgHP9wmNx8i2573RjUqin5s';
+    public const BOT_TOKEN = '682186483:AAEfr0p9f2wzYvtA6ay816aQ3HJnG7kW77E';
     public const DATABASE_NAME = 'id8115261_telegrambot';
     public const DATABASE_USER = 'id8115261_werewolfbot';
     public const DATABASE_PASSWORD = '*c39yegA$q9g';
