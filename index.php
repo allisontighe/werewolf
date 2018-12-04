67d508c5b6aba8fa2a2cc8b5d38a7a42b4df1d69
ArcTLK <adityanair313@gmail.com>
2018-12-04 18:26:29 +0530

    removed bot echo

--- index.php
+++ index.php
@@ -12,10 +12,7 @@ session_start();
 
 $content = file_get_contents('php://input');
 $update = json_decode($content, true);
-if (!$update) {
-    echo "<h1>Telegram Bot</h1>";
-    exit;
-}
+
 if (isset($update['message'])) {
     (new WerewolfBot($update['message']))->process();
 }
