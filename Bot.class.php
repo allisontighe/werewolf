f902df65da88857e9504c9f5714efb50d6c23f70
ArcTLK <adityanair313@gmail.com>
2018-12-04 19:57:25 +0530

    fix

--- Bot.class.php
+++ Bot.class.php
@@ -12,7 +12,7 @@ class Bot {
         $this->messageText = $message['text'] ?? 0;
     }
     private function send($method, $parameters) {
-        $curl = curl_init('https://api.telegram.com/bot'.Config::BOT_TOKEN.'/'.$method);
+        $curl = curl_init('https://api.telegram.org/bot'.Config::BOT_TOKEN.'/'.$method);
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
@@ -28,6 +28,6 @@ class Bot {
         echo json_encode($parameters);
     }
     protected function sendMessage($message) {
-        $this->sendEcho('sendMessage', ['chat_id' => $this->chatId, 'text' => $message]);
+        $this->send('sendMessage', ['chat_id' => $this->chatId, 'text' => $message]);
     }
 }
