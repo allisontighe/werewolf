eea416de65e7041cb847131604a4382b1f7833d1
allisontighe <allisontighe592@gmail.com>
2018-12-04 11:03:04 -0600

    Test's

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ require_once 'WerewolfBot.class.php';
 
 $content = file_get_contents('php://input');
 $update = json_decode($content, true);
-
+if(!$update){
+    echo "Werewolf Bot";
+    Exit()
+}
 if (isset($update['message'])) {
     (new WerewolfBot($update['message']))->process();
 }
