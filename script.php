9abf162e21ad3c29bd1127f1c6d02b85f316dc6e
ArcTLK <adityanair313@gmail.com>
2018-12-05 14:04:51 +0530

    fix

--- script.php
+++ script.php
@@ -3,7 +3,7 @@ require_once 'Connection.class.php';
 if ($_GET['key'] === 'applesarefruits' && $_GET['action'] === 'createtable') {
     try {
         $conn = new Connection;
-        $conn->exec("CREATE TABLE IF NOT EXISTS players ( id INT NOT NULL AUTO_INCREMENT , chatId BIGINT NOT NULL , lastAction TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , role TINYINT UNSIGNED NOT NULL DEFAULT '0' , PRIMARY KEY (id), INDEX (chatId), INDEX (role));");
+        $conn->exec("CREATE TABLE IF NOT EXISTS players ( id SERIAL PRIMARY KEY, chatId BIGINT, lastAction TIMESTAMP DEFAULT CURRENT_TIMESTAMP , role SMALLINT DEFAULT '0');");
         echo 'Script ran';
     }
     catch(PDOException $e) {
