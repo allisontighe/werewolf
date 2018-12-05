531ebcfd6cdc8d627803ff220cc5a722e35f003e
ArcTLK <adityanair313@gmail.com>
2018-12-05 14:10:40 +0530

    truncate

--- script.php
+++ script.php
@@ -3,8 +3,7 @@ require_once 'Connection.class.php';
 if ($_GET['key'] === 'applesarefruits' && $_GET['action'] === 'createtable') {
     try {
         $conn = new Connection;
-        $conn->query('INSERT INTO players (chatId) VALUES (100)');
-        $conn->query('INSERT INTO players (chatId) VALUES (1212)');
+        $conn->query('TRUNCATE TABLE players');
         $resultSet = $conn->query('SELECT * FROM players')->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($resultSet);
     }
