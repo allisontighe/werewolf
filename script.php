e82413a56fde6050709e25a6302bc8bf7f6ba2a6
ArcTLK <adityanair313@gmail.com>
2018-12-05 13:49:19 +0530

    try/catch

--- script.php
+++ script.php
@@ -1,9 +1,14 @@
 <?php
 require_once 'Connection.class.php';
 if ($_GET['key'] === 'applesarefruits' && $_GET['action'] === 'createtable') {
-    $conn = new Connection;
-    $conn->exec("CREATE TABLE `players` ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `chatId` BIGINT NOT NULL , `lastAction` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `role` TINYINT UNSIGNED NOT NULL DEFAULT '0' , PRIMARY KEY (`id`), INDEX (`chatId`), INDEX (`role`));");
-    echo 'Script ran';
+    try {
+        $conn = new Connection;
+        $conn->exec("CREATE TABLE IF NOT EXISTS `players` ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `chatId` BIGINT NOT NULL , `lastAction` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `role` TINYINT UNSIGNED NOT NULL DEFAULT '0' , PRIMARY KEY (`id`), INDEX (`chatId`), INDEX (`role`));");
+        echo 'Script ran';
+    }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+    }
 }
 else {
     echo 'Scripts page';
