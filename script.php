0a399984506e5dbd355e56059e20759779933a4c
ArcTLK <adityanair313@gmail.com>
2018-12-05 14:09:02 +0530

    testing inserts

--- script.php
+++ script.php
@@ -3,8 +3,10 @@ require_once 'Connection.class.php';
 if ($_GET['key'] === 'applesarefruits' && $_GET['action'] === 'createtable') {
     try {
         $conn = new Connection;
-        $conn->exec("CREATE TABLE IF NOT EXISTS players ( id SERIAL PRIMARY KEY, chatId BIGINT, lastAction TIMESTAMP DEFAULT CURRENT_TIMESTAMP , role SMALLINT DEFAULT '0');");
-        echo 'Script ran';
+        $conn->query('INSERT INTO players (chatId) VALUES (100)');
+        $conn->query('INSERT INTO players (chatId) VALUES (1212)');
+        $resultSet = $conn->query('SELECT * FROM players')->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($resultSet);
     }
     catch(PDOException $e) {
         echo $e->getMessage();
