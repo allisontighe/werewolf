88505848b9506785a8c3cc92548fec37fbba5b2b
ArcTLK <adityanair313@gmail.com>
2018-12-05 17:31:02 +0530

    updated column names

--- functions.php
+++ functions.php
@@ -1,19 +1,19 @@
 <?php
 function doesChatIdExist(Connection $connection, int $chatId): bool {
-    $PDOStatement = $connection->prepare('SELECT COUNT(chatId) FROM players WHERE chatId = ?');
+    $PDOStatement = $connection->prepare('SELECT COUNT(chat_id) FROM players WHERE chat_id = ?');
     $PDOStatement->execute([$chatId]);
     return boolval($PDOStatement->fetchColumn());
 }
 function doesTelegramIdExist(Connection $connection, int $chatId, int $telegramId): bool {
-    $PDOStatement = $connection->prepare('SELECT COUNT(telegramId) FROM players WHERE chatId = ? AND telegramId = ?');
+    $PDOStatement = $connection->prepare('SELECT COUNT(telegram_id) FROM players WHERE chat_id = ? AND telegram_id = ?');
     $PDOStatement->execute([$chatId, $telegramId]);
     return boolval($PDOStatement->fetchColumn());
 }
 function deleteChatId(Connection $connection, int $chatId) {
-    $PDOStatement = $connection->prepare('DELETE FROM players WHERE chatId = ?');
+    $PDOStatement = $connection->prepare('DELETE FROM chats WHERE chat_id = ?');
     $PDOStatement->execute([$chatId]);
 }
 function addToGame(Connection $connection, int $chatId, int $telegramId, string $name) {
-    $PDOStatement = $connection->prepare('INSERT INTO players (chatId, telegramId, name) VALUES (?, ?, ?)');
+    $PDOStatement = $connection->prepare('INSERT INTO players (chat_id, telegram_id, name) VALUES (?, ?, ?)');
     $PDOStatement->execute([$chatId, $telegramId, $name]);
 }
