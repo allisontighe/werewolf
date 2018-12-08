07d751b4ca066f86fcf261b9b100c606e72e7e0b
ArcTLK <adityanair313@gmail.com>
2018-12-08 23:20:29 +0530

    fix

--- Game.php
+++ Game.php
@@ -3,7 +3,7 @@ require_once 'Connection.class.php';
 require_once 'Game.class.php';
 ignore_user_abort(true);
 set_time_limit(100);
-if (isset($_GET['chat_id'])) exit('No data!');
+if (!isset($_GET['chat_id'])) exit('No data!');
 else {
     //start new game
     new Game(new Connection, $_GET['chat_id']);
