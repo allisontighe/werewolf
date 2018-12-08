088cb5bbd4960b9ca90e7f171c682a7c0593cdf0
allisontighe <allisontighe592@gmail.com>
2018-12-08 09:29:00 -0600

    update roles add new role: Drunk

--- WerewolfBot.class.php
+++ WerewolfBot.class.php
@@ -10,8 +10,9 @@ class WerewolfBot extends Bot {
     private $baddies = 0;
     private function loadRoles() {
         $this->roles[RoleId::villager] = new Role(RoleId::villager, 'Villager', false, taskTypes::none, 'The village plower.');
-        $this->roles[RoleId::werewolf] = new Role(RoleId::werewolf, 'Werewolf', true, taskTypes::night, 'The everyday baddie.');
+        $this->roles[RoleId::werewolf] = new Role(RoleId::werewolf, 'Werewolf', true, taskTypes::night, 'Stalking your prey at night you kill and devour the bodies of the villagers one by one.');
         $this->roles[RoleId::clown] = new Role(RoleId::clown, 'Clown', false, taskTypes::none, 'You are the Village clown, you play pranks on the villagers at night and though you are good, you are sometimes mistaken for bad');
+        $this->roles[RoleId::Drunk] = new Role(RoleId::Drunk, 'Drunk', false, taskTypes::none, 'You are the village drunk, too drunk to do anything at night');
     }
     public function __construct(array $message) {
         $this->connection = new Connection;
@@ -151,6 +152,9 @@ class WerewolfBot extends Bot {
             if($this->roles[$player['role']]->getTaskType() === taskTypes::night) {
                 if ($player['role'] === RoleId::werewolf) {
                     $this->sendMessageToPlayer('Who do you want to eat tonight?', $player['telegram_id'], generateKeyboard($player, $players, 'eat'));
+                    if ($player['role'] === RoleId::Clown) {
+                        $this->sendMessageToPlayer('Who do you want to prank tonight', $player['telegram_id'], generateKeyboard($player, $players, 'prank'));
+                    }
                 }
             }
         }
