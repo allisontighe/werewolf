b4498d90db4c2b53fd4a202842290a6061dc6deb
ArcTLK <adityanair313@gmail.com>
2018-12-08 23:28:17 +0530

    increased limit to 30 mins

--- Game.php
+++ Game.php
@@ -2,7 +2,7 @@
 require_once 'Connection.class.php';
 require_once 'Game.class.php';
 ignore_user_abort(true);
-set_time_limit(500);
+set_time_limit(1800);
 if (!isset($_GET['chat_id'])) exit('No data!');
 else {
     //start new game
